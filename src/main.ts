d3c56e0be2758e6002ed886d9370cf0ba5f292d2
Meshach Joseph <pstmeshach@gmail.com>
2020-09-03 02:46:20 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const app: NestExpressApplication = await NestFactory.create(AppModule);
   app.enableCors();
 
   const port = process.env.Port;
-  // const options = new DocumentBuilder()
-  //   .setTitle('ResidentApi')
-  //   .setDescription('Resident Management System')
-  //   .setVersion('1.0')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, options);
-  //SwaggerModule.setup('api', app, document);
-  //SwaggerModule.setup('/api', app, document);
-  //await app.listen(port);
   await app.listen(process.env.PORT || 3000);
-  // app.useStaticAssets(join(__dirname, '/static'), {
-  //   prefix: '/api',
-  // });
 }
 bootstrap();

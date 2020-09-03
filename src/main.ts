63992a39f04c644c5ce9d21d09774898ee22d25a
unknown <pstmeshach@gmail..com>
2020-09-03 13:35:05 +0100

    update to main

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ async function bootstrap() {
   app.enableCors();
 
   const port = process.env.Port;
+  const options = new DocumentBuilder()
+    .setTitle('ResidentApi')
+    .setDescription('Resident Management System')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
